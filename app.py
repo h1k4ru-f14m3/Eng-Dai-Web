06bb1d4f328e8fbb32a381a8d47c5971dd20e21a
MSI\CYBORG <hikaru.hino.flame@gmail.com>
2025-06-23 20:47:25 +0630

    Added admin panel and edit modal popup

--- app.py
+++ app.py
@@ -4,26 +4,46 @@ import sqlite3
 app = Flask(__name__)
 app.config["TEMPLATE_AUTO_RELOAD"] = True
 
-@app.route("/")
-def index():
+db_file = "data.db"
+
+def get_all(order_by):
     db = sqlite3.connect("data.db")
     db_cur = db.cursor()
-    db_cur.execute("SELECT eng, dai FROM data ORDER BY eng ASC")
-    datalist = db_cur.fetchall()
-    return render_template("index.html", data=datalist)
+    
+    db_cur.execute(f"SELECT id, eng, dai FROM data ORDER BY {order_by} ASC")
+    return db_cur.fetchall()
+
+@app.route("/")
+def index():
+    return render_template("index.html", data=get_all("eng"))
+
+@app.route("/admin")
+def admin_panel():
+    return render_template("admin.html", data=get_all("eng"))
+
+@app.route("/update", methods=["GET", "POST"])
+def update():
+    if request.method == "POST":
+        for i in get_all('eng'):
+            print(f"{i[0]} = {i[1]}")
+        return redirect("/")
+    
+@app.route("/dosomething")
+def dosomething():
+    return render_template("admin.html", data=get_all('eng'))
 
 @app.route("/search")
 def search():
-    db = sqlite3.connect("data.db")
+    db = sqlite3.connect(db_file)
     db_cur = db.cursor()
     q = request.args.get("q")
 
     if q == "":    
-        db_cur.execute("SELECT eng, dai FROM data ORDER BY eng ASC")
+        db_cur.execute("SELECT id, eng, dai FROM data ORDER BY eng ASC")
         datalist = db_cur.fetchall()
         return render_template("search.html", results=datalist)
 
-    db_cur.execute("SELECT eng, dai FROM data WHERE eng LIKE ? ORDER BY eng ASC", ["%" + q + "%"])
+    db_cur.execute("SELECT id, eng, dai FROM data WHERE eng LIKE ? ORDER BY eng ASC", [q + "%"])
     datalist = db_cur.fetchall()
     return render_template("search.html", results=datalist)
 
